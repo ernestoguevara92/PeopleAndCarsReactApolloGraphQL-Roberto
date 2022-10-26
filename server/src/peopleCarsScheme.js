b920a04b3a8ea081202661e107da1ac555a3cc47
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-25 20:30:17 -0700

    final tests

--- server/src/peopleCarsScheme.js
+++ server/src/peopleCarsScheme.js
@@ -103,10 +103,10 @@ const typeDefs = gql`
 
   type Car {
     id: String!
-    year: String
+    year: Int
     make: String
     model: String
-    price: String
+    price: Float
     personId: String!
   }
 
@@ -124,8 +124,8 @@ const typeDefs = gql`
     updatePerson(id: String!, firstName: String, lastName: String): Person
     removePerson(id: String!): Person
 
-    addCar(id: String!, year: String!, make: String!, model: String!, price: String!, personId: String!): Car
-    updateCar(id: String!, year: String, make: String, model: String, price: String, personId: String): Car
+    addCar(id: String!, year: Int!, make: String!, model: String!, price: Float!, personId: String!): Car
+    updateCar(id: String!, year: Int, make: String, model: String, price: Float, personId: String): Car
     removeCar(id: String!): Car
     removePersonCars(personId: String!): [Car]
   }
